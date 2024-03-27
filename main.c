94dd6dc32bcd81160417ebc9e962a0df035a958a
TrungLX <trungx1204@gmail.com>
2024-03-27 22:13:11 +0700

    update

--- main.c
+++ main.c
@@ -10,34 +10,36 @@
 
 
 
-void set_SensorConfig(SensorConfig *config, int i2c_bus, int sensor_addr, char* xshut_pin);
-{
-    config->i2c_bus = I2C_DEVICE;
-    config->sensor_addr = I2C_ADDR;
-    config->xshut_pin = XSHUT_PIN;
-}
+// void set_Sensor(Sensor_Config *config, int i2c_bus, int sensor_addr, char* xshut_pin);
+// {
+//     config->i2c_bus = I2C_BUS;
+//     config->sensor_addr = SENSOR_ADDR;
+//     config->xshut_pin = XSHUT_PIN;
+// }
 void active_sensor(){
     int fd = open(XSHUT_PIN, O_WRONLY);
     if (fd < 0)
     {
         perror("Failed to open GPIO Xshut pin.");
         exit(1);
-        write(fd, "0", 1);
-        usleep(10000);
+        // write(fd, "0", 1);
+        // usleep(10000);
         write(fd, "1", 1);
         usleep(10000);
         close(fd);
     }
 }
 
-void read_data_sensor(SensorConfig *config)
+void read_data_sensor(Sensor_Config *config)
 {
     unsigned char buf[2];
     buf[0] = 0x00;
     buf[1] = 0x01;
+    int file;
 
     // Mo I2C
-    if((file = open(config->i2c_bus, O_RDWR)) < 0)
+    file = open(config->i2c_bus, O_RDWR);
+    if(file < 0)
     {
         perror("Failed to open I2C bus");
         pthread_exit(NULL);
@@ -71,13 +73,15 @@ void read_data_sensor(SensorConfig *config)
 
 int main()
 {
-    pthread thread;
+    Sensor_Config sensor;
+    // set_Sensor(&sensor);
+    pthread_t thread_sensor;
 
-    if(pthread_create(&thread, NULL, read_data_sensor, NULL) != 0)
+    if(pthread_create(&thread_sensor, NULL, read_data_sensor(&sensor), NULL) != 0)
     {
         perror("Failed to create thread");
         return 1;
     }
-    pthread_join(thread, NULL);
+    pthread_join(thread_sensor, NULL);
     return 0;
 }
--- sensor.h
+++ sensor.h
@@ -1,20 +1,20 @@
 #ifndef SENSOR_H
 #define SENSOR_H
 
-#define I2C_DEVICE "/dev/i2c-2"
-#define I2C_ADDR 0x52 
+#define I2C_BUS "/dev/i2c-2"
+#define SENSOR_ADDR 0x52 
 #define XSHUT_PIN "/sys/class/gpio/gpio40/value"
 
 
 struct sensor_config
 {
-    int i2c_bus;
-    int sensor_addr;
-    char* xshut_pin;
-} SensorConfig;
+    int i2c_bus = I2C_BUS;
+    int sensor_addr = SENSOR_ADDR;
+    char* xshut_pin = XSHUT_PIN;
+} Sensor_Config;
 
-void set_SensorConfig(SensorConfig *config, int i2c_bus, int sensor_addr, char* xshut_pin);
+// void set_Sensor(Sensor_Config *config, int i2c_bus, int sensor_addr, char* xshut_pin);
 void active_sensor();
-void read_data_sensor(SensorConfig *config);
+void read_data_sensor(Sensor_Config *config);
 
 #endif 
